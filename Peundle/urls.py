9d3ae0cfe8c671a1569a8fc098eabc7b9e5d8db4
Lumi1919 <shensanlumi@gmail.com>
2023-10-27 22:14:47 +0100

    first commit

--- Peundle/urls.py
+++ Peundle/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('peundle', include('home.urls')),
+    path('.', include('home.urls')),
     path('', include('main.urls')),
     path('admin19199266/', admin.site.urls)
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
